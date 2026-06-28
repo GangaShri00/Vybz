10f323d1fe79a1f5ce8632af7861e889e69e45b9
GangaShri00 <gangashriar@gmail.com>
2026-06-28 22:52:37 +0530

    Phase 1: Project structure setup - Vybz

---
+++ frontend/vybz-app/src/app/components/seat-map/seat-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seat-map',
+  imports: [],
+  templateUrl: './seat-map.component.html',
+  styleUrl: './seat-map.component.css'
+})
+export class SeatMapComponent {
+
+}
